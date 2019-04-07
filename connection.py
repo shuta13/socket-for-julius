84b5e3826df0aa1a0f46876d6816ad132504c51b
shuta13 <hollystarsun@gmail.com>
2019-04-08 05:30:17 +0900

    fixed

--- connection.py
+++ connection.py
@@ -11,22 +11,24 @@ port = 10500
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((host, port))
 
-data =""
+data = ""
 while True:
 
-    while ( string.find(data, "\n.") == -1 ):
-        data = data + sock.recv(1024)
+  while (string.find(data, "\n.") == -1 ):
+    data = data + sock.recv(1024)
     
-    strTemp = ""
-    for line in data.split('\n'):
-        index = line.find('WORD="')
-        if index != -1:
-            line = line[index+6:line.find('"', index+6)]
-            strTemp = strTemp + line
-    if (strTemp):
-        f = open('input.txt', 'w')
-        f.write(strTemp)
-        f.close
-        print( strTemp )
-        break
-    data = ""
+  strTemp = ""
+  
+  for line in data.split('\n'):
+    index = line.find('WORD="')
+    if index != -1:
+      line = line[index+6:line.find('"', index+6)]
+      strTemp = strTemp + line
+
+  if (strTemp):
+    f = open('input.txt', 'w')
+    f.write(strTemp)
+    f.close
+    print( strTemp )
+    break
+  data = ""
